c9867cfe771ca4a13144a7c7290adf15eca18c42
giacomotolari <tolarigiacomo@gmail.com>
2025-01-02 21:19:42 +0100

    feat: add Skill model with name field and related projects and companies properties

--- django_project/skills/models.py
+++ django_project/skills/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def projects(self):
+        return self.project_set.all()
+
+    @property
+    def companies(self):
+        return self.company_set.all()
