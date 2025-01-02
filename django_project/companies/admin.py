ae937bbfea88f8eb44547715361f1a1f296d0875
giacomotolari <tolarigiacomo@gmail.com>
2025-01-02 16:49:36 +0100

    feat: add EmployerCompany and CustomerCompany models with admin registration

--- django_project/companies/admin.py
+++ django_project/companies/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import EmployerCompany, CustomerCompany
 
-# Register your models here.
+admin.site.register(EmployerCompany)
+admin.site.register(CustomerCompany)
--- django_project/companies/models.py
+++ django_project/companies/models.py
@@ -1,3 +1,31 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class TypeChoices(models.TextChoices):
+    AGENCY = ("agency", "Agency")
+    PRODUCT_COMPANY = ("product-company", "Product Company")
+    SCHOOL = ("school", "School")
+    STARTUP = ("startup", "Startup")
+    CORPORATE = ("corporate", "Corporate")
+
+
+class Company(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, db_index=True, unique=True)
+    name = models.CharField(max_length=255)
+    url = models.URLField(null=True, blank=True)
+    company_type = models.CharField(max_length=255, choices=TypeChoices.choices)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class EmployerCompany(Company):
+    pass
+
+
+class CustomerCompany(Company):
+    pass
